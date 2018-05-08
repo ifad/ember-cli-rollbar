14fb6d1c2bffb3a2f939d873bf199b85b49fbd8e
Paolo Zaccagnini <paolo@pzac.net>
2018-05-08 12:42:37 +0200

    Amend update snippet task

--- tasks/update-snippet.sh
+++ tasks/update-snippet.sh
@@ -9,12 +9,13 @@ if $(git diff-index --quiet --cached HEAD); then
   echo "Committing changes ..."
   version=$(curl --silent $packageurl | grep 'version": "' | head -n 1 | cut -d '"' -f 4)
   git add addon/snippet.js
+  git add client/rollbar.js
   git commit -m "update rollbar snippet to version $version"
-  echo "Publishing changes ..."
-  npm version patch -m "update rollbar snippet to version $version"
-  git push origin master
-  git push origin --tags
-  npm publish
+  # echo "Publishing changes ..."
+  # npm version patch -m "update rollbar snippet to version $version"
+  # git push origin master
+  # git push origin --tags
+  # npm publish
 else
   echo "Git index is dirty, skipping git commit and npm publish steps"
 fi
